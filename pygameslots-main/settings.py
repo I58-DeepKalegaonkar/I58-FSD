5d4a5c4e1b18232bdf243995bf8fb7dd51f883ff
I58-DeepKalegaonkar <1032220941@mitwpu.edu.in>
2024-08-24 17:34:56 +0530

    This is AIES
    AIES  mini project codes and stuff

---
+++ pygameslots-main/settings.py
@@ -0,0 +1,28 @@
+# Display settings
+DEFAULT_IMAGE_SIZE = (300, 300)
+FPS = 120
+HEIGHT = 1000
+WIDTH = 1600
+START_X, START_Y = 0, -300
+X_OFFSET, Y_OFFSET = 20, 0
+
+# Images
+BG_IMAGE_PATH = 'graphics/0/bg.png'
+GRID_IMAGE_PATH = 'graphics/0/gridline.png'
+GAME_INDICES = [1, 2, 3] # 0 and 4 are outside of play area
+SYM_PATH = 'graphics/0/symbols'
+
+# Text
+TEXT_COLOR = 'White'
+UI_FONT = 'graphics/font/Kidspace-DEMO.ttf'
+UI_FONT_SIZE = 30
+WIN_FONT_SIZE = 110
+
+# 4 Symbols (for wins)
+symbols = {
+    'diamond': f"{SYM_PATH}/0_diamond.png", 
+    'floppy': f"{SYM_PATH}/0_floppy.png",
+    'hourglass': f"{SYM_PATH}/0_hourglass.png",
+    'hourglass2': f"{SYM_PATH}/0_hourglass.png",
+    'telephone': f"{SYM_PATH}/0_telephone.png"
+}
